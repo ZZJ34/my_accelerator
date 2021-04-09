797c7000fafab083bc6abdca3d20b6cdea997599
zzj34 <zhaozjseu@163.com>
2021-04-09 08:39:12 +0800

    添加新的模块设计

---
+++ sources_1/new/get_data.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/04/09 08:36:15
+// Design Name: 取数据
+// Module Name: get_data
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+//            根据 get_param 模块传送的参数（i，z，k，l）从 rom 取出对应的数据
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module get_data(
+    input rst_n,               // 复位信号
+
+    input [2:0] en_get_param,  // 该模块使能 
+    );
+endmodule
